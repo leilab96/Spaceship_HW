86358776e599225437e7ba525bbdf86d21f1a02b
Leila <badeeb9611@gmail.com>
2022-11-27 13:28:48 +0100

    Spaceship

--- spaceship.js
+++ spaceship.js
@@ -1,24 +1,27 @@
+//object-el elkészítve
 let spaceShip = {
   Fuel: 400,
   Passengers: ["John", "Steve", "Sam", "Danielle"],
   Shields: true,
   Speedometer: 0,
-
+  //kilistázza az utasokat
   listPassengers: function () {
     for (let i = 0; i < this.Passengers.length; i++) {
       console.log("Passenger " + (i + 1) + ": " + this.Passengers[i]);
     }
   },
+  //hozzáad egy új utast
   addPassenger: function (newPassenger) {
     this.Passengers.push(newPassenger);
     console.log(newPassenger + " was added to the ship");
   },
+  //a megadott távolságot utazik és annak megfelelően változtatja a paramétereket.
   travel: function (distance) {
     console.log("Trying to travel: " + distance);
     if (this.Fuel === 0) {
       console.log("Can't go further, tank is empty!");
     } else {
-      let newFuel = this.Fuel - distance / 2;
+      let newFuel = this.Fuel - distance / 2; //Fuel distance felével csökken utazáskor
       if (newFuel < 0) {
         distance = 2 * this.Fuel;
         newFuel = 0;
--- spaceship_class.js
+++ spaceship_class.js
@@ -1,28 +1,31 @@
+//class-al elkészítve a Spaceship és utána pédányosítva
+
 class SpaceShip {
+  //Fuel és Passengers kell példányosításnál megadni, Shields és Speedometer alapértelmezett
   constructor(Fuel, Passengers) {
     this.Fuel = Fuel;
     this.Passengers = Passengers;
     this.Shields = true;
     this.Speedometer = 0;
   }
-
+  //kilistázza az utasokat
   listPassengers() {
     for (let i = 0; i < this.Passengers.length; i++) {
       console.log("Passenger " + (i + 1) + ": " + this.Passengers[i]);
     }
   }
-
+  //hozzáad egy új utast
   addPassenger(newPassenger) {
     this.Passengers.push(newPassenger);
     console.log(newPassenger + " was added to the ship");
   }
-
+  //a megadott távolságot utazik és annak megfelelően változtatja a paramétereket
   travel(distance) {
     console.log("Trying to travel: " + distance);
     if (this.Fuel === 0) {
       console.log("Can't go further, tank is empty!");
     } else {
-      let newFuel = this.Fuel - distance / 2;
+      let newFuel = this.Fuel - distance / 2; //Fuel distance felével csökken utazáskor
       if (newFuel < 0) {
         distance = 2 * this.Fuel;
         newFuel = 0;
